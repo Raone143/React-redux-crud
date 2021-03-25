acd1fcc9e64f8b5929efb5809143873a948d38dc
Raone143 <sekhar.raj43@gmail.com>
2021-03-25 19:01:56 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react"
+import './App.css';
+import TransactionList  from './components/TransactionList'
+function App() {
+  return (
+   <TransactionList />
+  );
+}
+
+export default App;
---
+++ src/components/TransactionList.js
@@ -0,0 +1,72 @@
+import React,{Component} from "react"
+import TransactionForm from './TransactionForm'
+import { connect } from 'react-redux'
+import * as actions from '../actions/transactionAction'
+import { bindActionCreators } from "redux"
+
+class TransactionList extends Component {
+    state = {
+        currentIndex:-1,
+        list:this.returnList()
+    }
+    returnList() {
+        if(localStorage.getItem('transactions')==null)
+        localStorage.setItem('transactions',JSON.stringify([]))
+        return JSON.parse(localStorage.getItem('transactions'))
+    }
+    onAddorEdit =(data) => {
+        var list = this.returnList()
+        list.push(data)
+        localStorage.setItem('transactions',JSON.stringify(list))
+        this.setState({list})
+        
+    }
+    handleEdit = index => {
+      this.props.updateTransactionIndex(index)
+    }
+    handleDelete = index => {
+     this.props.deleteTransaction(index) 
+    }
+    
+
+    render() {
+        return (
+            <div>
+            <TransactionForm />
+            <hr />
+            <table>
+                <tbody>
+                    {
+                        this.props.list.map((item,index) => {
+                            return <tr key={index}>
+                                <td>{item.bAccountNo}</td>
+                                <td>{item.iFSC}</td>
+                                <td>{item.bName}</td>
+                                <td>{item.amount}</td>
+                                <td><button onClick={()=>this.handleEdit(index)}>Edit</button></td>
+                                <td><button onClick={()=>this.handleDelete(index)}>Delete</button></td>
+                            </tr>
+                        })
+                    }
+                </tbody>
+            </table>
+            </div>
+          )
+    }
+}
+const mapStateToProps = state =>{
+    return {
+        list:state.list
+    }
+}
+
+const mapDispatchToProps = dispatch => {
+    return bindActionCreators({
+        deleteTransaction : actions.Delete,
+        updateTransactionIndex:actions.UpdateIndex
+    },dispatch)
+}
+export default connect(mapStateToProps,mapDispatchToProps)(TransactionList)
+
+
+
